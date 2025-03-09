64dafbca3189c8da9aedfc7b867750bf650e9b2f
Bartu Baş <bartush@Mac.home>
2025-03-09 09:00:23 +0300

    Code for entity struct

---
+++ Assets/Code/EntityManager/Entity.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace CodePractice
+{
+    public struct Entity : IEquatable<Entity>
+    {
+        public int Index;
+        public int Version;
+
+        public static readonly Entity Null = new Entity();
+
+        public static bool operator ==(Entity lhs, Entity rhs)
+        {
+            return lhs.Index == rhs.Index && lhs.Version == rhs.Version;
+        }
+
+        public static bool operator !=(Entity lhs, Entity rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public bool Equals(Entity other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Entity other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Index, Version);
+        }
+    }
+}
