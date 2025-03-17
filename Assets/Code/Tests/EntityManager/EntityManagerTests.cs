24c69940dfcc4426e244fa1cd106adbe357c3396
Bartu Baş <bartush@Mac.home>
2025-03-17 06:35:44 +0300

    Fix: Don't leak memory on tests

--- Assets/Code/Tests/EntityManager/EntityManagerTests.cs
+++ Assets/Code/Tests/EntityManager/EntityManagerTests.cs
@@ -4,13 +4,24 @@ namespace CodePractice.Tests
 {
     public class EntityManagerTests
     {
-        // TODO: Setup, Teardown (don't leak memory)
-        
+        private EntityManager _em;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _em = EntityManager.Create();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _em.Dispose();
+        }
+
         [Test]
         public void CreateEntity()
         {
-            var em = EntityManager.Create(); 
-            var entity = em.CreateEntity();
+            var entity = _em.CreateEntity();
 
             Assert.AreEqual(1, entity.Index);
             Assert.AreEqual(0, entity.Version);
@@ -19,12 +30,11 @@ namespace CodePractice.Tests
         [Test]
         public void IndexGoesUp()
         {
-            var em = EntityManager.Create();
             var baseIdx = 1;
 
             for (int i = 0; i < 100_000; i++)
             {
-                var entity = em.CreateEntity();
+                var entity = _em.CreateEntity();
                 Assert.AreEqual(baseIdx + i, entity.Index);
                 Assert.AreEqual(0, entity.Version);
             }
@@ -33,15 +43,14 @@ namespace CodePractice.Tests
         [Test]
         public void VersionGoesUp()
         {
-            var em = EntityManager.Create();
             var baseVer = 1;
-            var entity = em.CreateEntity();
+            var entity = _em.CreateEntity();
 
             for (int i = 0; i < 100_000; i++)
             {
-                em.DestroyEntity(entity);
-                entity = em.CreateEntity();
-                
+                _em.DestroyEntity(entity);
+                entity = _em.CreateEntity();
+
                 Assert.AreEqual(1, entity.Index);
                 Assert.AreEqual(baseVer + i, entity.Version);
             }
@@ -50,18 +59,16 @@ namespace CodePractice.Tests
         [Test]
         public void DefaultEntityDoesNotExist()
         {
-            var em = EntityManager.Create();
             var entity = new Entity();
 
-            Assert.IsFalse(em.Exists(entity));
+            Assert.IsFalse(_em.Exists(entity));
         }
 
         [Test]
         public void CreateEntity2()
         {
-            var em = EntityManager.Create();
-            var entity1 = em.CreateEntity();
-            var entity2 = em.CreateEntity();
+            var entity1 = _em.CreateEntity();
+            var entity2 = _em.CreateEntity();
 
             Assert.AreEqual(2, entity2.Index);
             Assert.AreEqual(0, entity2.Version);
@@ -70,23 +77,21 @@ namespace CodePractice.Tests
         [Test]
         public void ReuseEntityIndex()
         {
-            var em = EntityManager.Create();
-            var entity1 = em.CreateEntity();
-            em.DestroyEntity(entity1);
-            var entity2 = em.CreateEntity();
-            
+            var entity1 = _em.CreateEntity();
+            _em.DestroyEntity(entity1);
+            var entity2 = _em.CreateEntity();
+
             Assert.AreEqual(1, entity2.Index);
             Assert.AreEqual(1, entity2.Version);
         }
-        
+
         [Test]
         public void ReuseEntityIndexThenCreate()
         {
-            var em = EntityManager.Create();
-            var entity1 = em.CreateEntity();
-            em.DestroyEntity(entity1);
-            var entity2 = em.CreateEntity();
-            var entity3 = em.CreateEntity();
+            var entity1 = _em.CreateEntity();
+            _em.DestroyEntity(entity1);
+            var entity2 = _em.CreateEntity();
+            var entity3 = _em.CreateEntity();
 
             Assert.AreEqual(2, entity3.Index);
             Assert.AreEqual(0, entity3.Version);
@@ -95,31 +100,28 @@ namespace CodePractice.Tests
         [Test]
         public void CreatedEntityExistsOnce()
         {
-            var em = EntityManager.Create();
-            var entity1 = em.CreateEntity();
-            
-            Assert.IsTrue(em.Exists(entity1));
+            var entity1 = _em.CreateEntity();
+
+            Assert.IsTrue(_em.Exists(entity1));
         }
 
         [Test]
         public void CreatedEntityExistsTwice()
         {
-            var em = EntityManager.Create();
-            var entity1 = em.CreateEntity();
-            var entity2 = em.CreateEntity();
-            
-            Assert.IsTrue(em.Exists(entity1));
-            Assert.IsTrue(em.Exists(entity2));
+            var entity1 = _em.CreateEntity();
+            var entity2 = _em.CreateEntity();
+
+            Assert.IsTrue(_em.Exists(entity1));
+            Assert.IsTrue(_em.Exists(entity2));
         }
 
         [Test]
         public void DestroyedEntityDoesNotExist()
         {
-            var em = EntityManager.Create();
-            var entity1 = em.CreateEntity();
-            em.DestroyEntity(entity1);
-            
-            Assert.IsFalse(em.Exists(entity1));
+            var entity1 = _em.CreateEntity();
+            _em.DestroyEntity(entity1);
+
+            Assert.IsFalse(_em.Exists(entity1));
         }
 
         [Test]
@@ -140,10 +142,9 @@ namespace CodePractice.Tests
         [Test]
         public void CanNotDestroyEntityTwice()
         {
-            var em = EntityManager.Create();
-            var entity = em.CreateEntity();
-            Assert.IsTrue(em.DestroyEntity(entity));
-            Assert.IsFalse(em.DestroyEntity(entity));
+            var entity = _em.CreateEntity();
+            Assert.IsTrue(_em.DestroyEntity(entity));
+            Assert.IsFalse(_em.DestroyEntity(entity));
         }
     }
 }
