d75a23d2ea0d6098adcc12ef9081fc83d0a1c09c
Bartu Baş <bartubas99@gmail.com>
2025-04-13 10:05:49 +0300

    Add ComponentType Equality Members

--- Assets/Code/EntityManager/ComponentType.cs
+++ Assets/Code/EntityManager/ComponentType.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace CodePractice
 {
-    public struct ComponentType
+    public struct ComponentType : IEquatable<ComponentType>, IComparable<ComponentType>
     {
         public int TypeIndex;
 
@@ -10,6 +12,37 @@ namespace CodePractice
             {
                 TypeIndex = TypeManager.GetTypeIndex<T>()
             };
+        }
+
+        public static bool operator ==(ComponentType left, ComponentType right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ComponentType left, ComponentType right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ComponentType other && Equals(other);
+        }
+
+
+        public bool Equals(ComponentType other)
+        {
+            return TypeIndex == other.TypeIndex;
+        }
+
+        public int CompareTo(ComponentType other)
+        {
+            return TypeIndex.CompareTo(other.TypeIndex);
+        }
+
+        public override int GetHashCode()
+        {
+            return TypeIndex;
         }
     }
 }
