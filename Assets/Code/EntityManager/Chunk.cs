5e44109f7e0c69809c78595350f87e3c65d49bc1
Bartu Baş <bartubas99@gmail.com>
2025-04-14 06:40:55 +0300

    Add Set Archetype

--- Assets/Code/EntityManager/Chunk.cs
+++ Assets/Code/EntityManager/Chunk.cs
@@ -1,21 +1,27 @@
 using System;
 using System.Runtime.CompilerServices;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace CodePractice
 {
-    public struct Chunk : IDisposable
+    public unsafe struct Chunk : IDisposable
     {
         // Chunk Layout
         // EntityIds
 
         public NativeArray<byte> Memory;
+        public NativeArray<int> ComponentOffsets;
+        public NativeArray<int> ComponentSizes;
         public int Count;
         public int Capacity;
 
         public const int KiB = 1024;
         public const int ChunkSize = 16 * KiB;
+
+        private Archetype _archetype;
         
+        // TODO: Probably should be clear memory, otherwise newly created entities will require mem-write (but do they already require it?)
         public static Chunk Create()
         {
             return new Chunk
@@ -26,6 +32,37 @@ namespace CodePractice
             };
         }
 
+        // TODO: You're left in here
+        // Calculate how many entities can fit (including the Entity size)
+        // Calculate offsets by type and save
+        // Adding entity -> Write to each individual array
+        public void SetArchetype(Archetype archetype)
+        {
+            var totalComponentSize = sizeof(Entity);
+            var componentCount = archetype.Components.Length;
+            ComponentOffsets = new NativeArray<int>(componentCount, Allocator.Persistent);
+            ComponentSizes = new NativeArray<int>(componentCount, Allocator.Persistent);
+            
+            for (int i = 0; i < componentCount; i++)
+            {
+                var comp = archetype.Components[i];
+                var size = TypeManager.GetTypeInfo(comp.TypeIndex).Size;
+                ComponentSizes[i] = size;
+                totalComponentSize += size;
+            }
+
+            Capacity = ChunkSize / totalComponentSize;
+
+            var baseOffset = sizeof(Entity) * Capacity;
+            ComponentOffsets[0] = baseOffset;
+            
+            for (int i = 1; i < componentCount; i++)
+            {
+                ComponentOffsets[i] = baseOffset + ComponentSizes[i - 1] * Capacity;
+            }
+            
+            _archetype = archetype;
+        }
         public void Dispose()
         {
             Memory.Dispose();
