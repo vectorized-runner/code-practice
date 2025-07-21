59053181fa605a0e9d996b3cb0255c62e52c7409
Bartu Baş <bartubas99@gmail.com>
2025-07-21 10:16:54 +0300

    Formatting, consecutive alloc tests

--- Assets/Code/Tests/Allocators/LinearAllocatorTests.cs
+++ Assets/Code/Tests/Allocators/LinearAllocatorTests.cs
@@ -3,146 +3,127 @@ using NUnit.Framework;
 
 namespace CodePractice.Tests
 {
-    // TODO: Don't leak memory (auto dispose pattern)
-    // with the Linear allocator
-    public unsafe class LinearAllocatorTests
-    {
-        [Test]
-        public void SizeMatch()
-        {
-            var allocator = new LinearAllocator(128);
-            Assert.AreEqual(128, allocator.Length);
-        }
+	public unsafe class LinearAllocatorTests
+	{
+		[Test]
+		public void SizeMatch()
+		{
+			using var allocator = new LinearAllocator(128);
+			Assert.AreEqual(128, allocator.Length);
+		}
 
-        [Test]
-        public void DefaultAllocFails()
-        {
-            var alloc = new LinearAllocator();
-            var ptr = alloc.Alloc(10);
-            
-            Assert.IsTrue(ptr == null);
-        }
+		[Test]
+		public void DefaultAllocFails()
+		{
+			using var alloc = new LinearAllocator();
+			var ptr = alloc.Alloc(10);
 
-        // TODO: This crashes, figure it out
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(4)]
-        [TestCase(10)]
-        [TestCase(16)]
-        [TestCase(32)]
-        [TestCase(64)]
-        [TestCase(99)]
-        [TestCase(1024)]
-        [TestCase(1_000_000)]
-        public void ExactSizeAllocDoesNotThrow(int size)
-        {
-            Assert.Throws<Exception>(() =>
-            {
-                using var allocator = new LinearAllocator(size);
-                allocator.Alloc(size);
-            });
-        }
-        
-        [TestCase(129)]
-        [TestCase(256)]
-        [TestCase(512)]
-        [TestCase(int.MaxValue)]
-        public void OutOfBoundsAllocThrows(int allocSize)
-        {
-            Assert.Throws<Exception>(() =>
-            {
-                using var allocator = new LinearAllocator(128);
-                allocator.Alloc(allocSize);
-            });
-        }
+			Assert.IsTrue(ptr == null);
+		}
 
-        [TestCase(0)]
-        [TestCase(-1)]
-        [TestCase(-2)]
-        [TestCase(-100)]
-        [TestCase(int.MinValue)]
-        public void NonPositiveAllocThrows(int size)
-        {
-            Assert.Throws<Exception>(() =>
-            {
-                using var alloc = new LinearAllocator(128);
-                alloc.Alloc(size);
-            });
-        }
+		[TestCase(1)]
+		[TestCase(2)]
+		[TestCase(4)]
+		[TestCase(10)]
+		[TestCase(16)]
+		[TestCase(32)]
+		[TestCase(64)]
+		[TestCase(99)]
+		[TestCase(1024)]
+		[TestCase(1_000_000)]
+		public void ExactSizeAllocDoesNotThrow(int size)
+		{
+			Assert.Throws<Exception>(() =>
+			{
+				using var allocator = new LinearAllocator(size);
+				allocator.Alloc(size);
+			});
+		}
 
-        [Test]
-        public void AlignForward_1()
-        {
-            Assert.AreEqual(32, (int)MemoryUtil.AlignForward((void*)32, 16));
-        }
-        
-        [Test]
-        public void AlignForward_2()
-        {
-            Assert.AreEqual(128, (int)MemoryUtil.AlignForward((void*)32, 128));
-        }
-        
-        [Test]
-        public void AlignForward_3()
-        {
-            Assert.AreEqual(64, (int)MemoryUtil.AlignForward((void*)64, 16));
-        }
-        
-        [Test]
-        public void AlignForward_4()
-        {
-            Assert.AreEqual(64, (int)MemoryUtil.AlignForward((void*)64, 16));
-        }
-        
-        [Test]
-        public void AlignForward_5()
-        {
-            Assert.AreEqual(32, (int)MemoryUtil.AlignForward((void*)27, 8));
-        }
+		[TestCase(129)]
+		[TestCase(256)]
+		[TestCase(512)]
+		[TestCase(int.MaxValue)]
+		public void OutOfBoundsAllocThrows(int allocSize)
+		{
+			Assert.Throws<Exception>(() =>
+			{
+				using var allocator = new LinearAllocator(128);
+				allocator.Alloc(allocSize);
+			});
+		}
 
-        [Test]
-        public static void IsPow_0()
-        {
-            Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(0));
-        }
-        
-        [Test]
-        public static void IsPow_1()
-        {
-            Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(-1));
-            Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(-2));
-            Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(-4));
-            Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(int.MinValue));
-        }
+		[TestCase(0)]
+		[TestCase(-1)]
+		[TestCase(-2)]
+		[TestCase(-100)]
+		[TestCase(int.MinValue)]
+		public void NonPositiveAllocThrows(int size)
+		{
+			Assert.Throws<Exception>(() =>
+			{
+				using var alloc = new LinearAllocator(128);
+				alloc.Alloc(size);
+			});
+		}
 
-        [Test]
-        public void InBoundsAllocDoesNotFail()
-        {
-            
-        }
+		[Test]
+		public void AlignForward_1()
+		{
+			Assert.AreEqual(32, (int)MemoryUtil.AlignForward((void*)32, 16));
+		}
 
-        [Test]
-        public void ExactSizeAllocDoesNotFail()
-        {
-            
-        }
+		[Test]
+		public void AlignForward_2()
+		{
+			Assert.AreEqual(128, (int)MemoryUtil.AlignForward((void*)32, 128));
+		}
 
-        [Test]
-        public void ConsecutiveAllocDoesNotFail()
-        {
-            
-        }
-        
-        [Test]
-        public void ZeroAllocFails()
-        {
-            // TODO:
-        }
+		[Test]
+		public void AlignForward_3()
+		{
+			Assert.AreEqual(64, (int)MemoryUtil.AlignForward((void*)64, 16));
+		}
 
-        [Test]
-        public void NegativeAllocFails()
-        {
-            // TODO:
-        }
-    }
+		[Test]
+		public void AlignForward_4()
+		{
+			Assert.AreEqual(64, (int)MemoryUtil.AlignForward((void*)64, 16));
+		}
+
+		[Test]
+		public void AlignForward_5()
+		{
+			Assert.AreEqual(32, (int)MemoryUtil.AlignForward((void*)27, 8));
+		}
+
+		[Test]
+		public static void IsPow_0()
+		{
+			Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(0));
+		}
+
+		[Test]
+		public static void IsPow_1()
+		{
+			Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(-1));
+			Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(-2));
+			Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(-4));
+			Assert.AreEqual(false, MemoryUtil.IsPowerOfTwo(int.MinValue));
+		}
+
+		[Test]
+		public void ConsecutiveAllocDoesNotFail()
+		{
+			Assert.DoesNotThrow(() =>
+			{
+				using var alloc = new LinearAllocator(32);
+				alloc.Alloc(1);
+				alloc.Alloc(10);
+				alloc.Alloc(9);
+				alloc.Alloc(5);
+				alloc.Alloc(7);
+			});
+		}
+	}
 }
