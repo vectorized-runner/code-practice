575fe7b32dc4d615552f2ec653e0c1b6d3968bca
Bartu Baş <bartubas99@gmail.com>
2025-08-31 11:17:44 +0300

    Trivial

--- Assets/Code/Tests/Algorithms/MergeSortTests.cs
+++ Assets/Code/Tests/Algorithms/MergeSortTests.cs
@@ -3,15 +3,10 @@ using UnityEngine;
 
 namespace Code.Algorithms.Tests
 {
-    public class MergeSortTests
+    public static class MergeSortTests
     {
         [Test]
-        public static void Sort_Empty()
-        {
-        }
-
-        [Test]
-        public static void Sort_1()
+        public static void MergeSort_1()
         {
             var arr = new int[] { -1000, 5000, 1, 435899, -23423489, 0, 4853475, 123424578, 0, -349284, 13, 5, 45264, 8123 }; 
             MergeSort.Sort(arr);
