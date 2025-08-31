5611c52ec8445df2ba12a7d3ffe6e2180759eb0f
Bartu Baş <bartubas99@gmail.com>
2025-08-31 11:20:53 +0300

    Add Random sort test

--- Assets/Code/Tests/Algorithms/MergeSortTests.cs
+++ Assets/Code/Tests/Algorithms/MergeSortTests.cs
@@ -6,6 +6,29 @@ namespace Code.Algorithms.Tests
     public static class MergeSortTests
     {
         [Test]
+        public static void RandomSortTest()
+        {
+            var arrayLength = 10_000;
+            var seed = 43589354u;
+            var random = new Unity.Mathematics.Random(seed);
+            var arr = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                arr[i] = random.NextInt();
+            }
+            
+            MergeSort.Sort(arr);
+            
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                Assert.IsTrue(arr[i] <= arr[i + 1]);
+            }
+            
+            Debug.Log(MergeSort.ArrToStr(arr));
+        }
+        
+        [Test]
         public static void MergeSort_1()
         {
             var arr = new int[] { -1000, 5000, 1, 435899, -23423489, 0, 4853475, 123424578, 0, -349284, 13, 5, 45264, 8123 }; 
