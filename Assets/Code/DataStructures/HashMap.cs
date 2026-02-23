df653f840dd35fd5112648e0b995a4c216774455
Bartu Baş <bartubas99@gmail.com>
2026-02-23 07:14:03 +0300

    Add ctor

--- Assets/Code/DataStructures/HashMap.cs
+++ Assets/Code/DataStructures/HashMap.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Mathematics;
 
 // Optimization Tricks:
 // SoA data storage (keys and values separate): Don't load value in RAM unnecessarily
@@ -11,8 +12,32 @@ public class HashMap<TKey, TValue> where TKey : IEquatable<TKey>
     // [empty | deleted | first 7 bits of hashcode]
     // Empty < 0, Full >= 0, Tombstone -> Not negative, 0xFE, 0xFF is used as sentinel
     private byte[] _metadataArray;
+    private int _length;
+    
+    public int Length => _length;
+    public int Capacity => _keys.Length;
+    
+    // TODO: Branchless search
+    // TODO: Single contiguous block allocation?
+    // TODO: backshift deletion
+    // TODO: SIMD probing window
+    // TODO: Implement resize
 
-    private int Capacity => _keys.Length;
+    private const float _maxLoadFactor = 0.9f;
+    private const int _minCapacity = 4;
+
+    public HashMap(int initialCapacity = _minCapacity)
+    {
+        if (initialCapacity < 0)
+        {
+            throw new Exception("InitialCapacity can't be negative.");
+        }
+
+        var capacity = math.ceilpow2(math.max(_minCapacity, initialCapacity));
+        _keys = new TKey[capacity];
+        _values = new TValue[capacity];
+        _metadataArray = new byte[capacity];
+    }
     
     public TValue this[TKey key]
     {
@@ -27,6 +52,7 @@ public class HashMap<TKey, TValue> where TKey : IEquatable<TKey>
         var hash = key.GetHashCode();
         // Fast modulo. Requirement: Capacity is power of two
         var idx = hash & (Capacity - 1);
+        
         
         throw new NotImplementedException();
     }
